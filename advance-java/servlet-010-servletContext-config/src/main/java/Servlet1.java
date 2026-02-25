60c97bb3e961cea542c5c6e3d70082c1c5a976bc
JavaBySandeep <42215856+javabysandeep@users.noreply.github.com>
2026-02-25 10:29:12 +0530

    httpsession

---
+++ advance-java/servlet-010-servletContext-config/src/main/java/Servlet1.java
@@ -0,0 +1,26 @@
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Servlet1 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        ServletContext context = getServletContext();
+        writer.println("ServletContext info");
+        writer.println(context.getAttribute("app-wide-username"));
+
+        ServletConfig config = getServletConfig();
+        writer.println("ServletConfig info");
+        writer.println(config.getInitParameter("s1-username"));
+
+
+    }
+}
